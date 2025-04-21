28de0f421f581a0272730b9cd73bc7daa3dc1836
rakellbandeira <rakelldesigner@gmail.com>
2025-04-21 16:05:27 -0400

    Other enhancements

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -9,4 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 300);
       }, 5000);
     });
+
+    const hamburger = document.getElementById('hamburger-menu');
+    const mainMenu = document.getElementById('main-menu');
+    
+    if (hamburger) {
+      hamburger.addEventListener('click', function() {
+        mainMenu.classList.toggle('show');
+      });
+    }
+    
+    // Close menu when clicking outside
+    document.addEventListener('click', function(event) {
+      if (!event.target.closest('.main-nav') && !event.target.closest('.hamburger')) {
+        if (mainMenu.classList.contains('show')) {
+          mainMenu.classList.remove('show');
+        }
+      }
+    });
+
   });
